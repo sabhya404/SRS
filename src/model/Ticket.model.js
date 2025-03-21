1c40c2321af4ef46bc25b142952b1dbcdf8eb7d7
codebash <agarwalsabhya44@gmail.com>
2025-03-21 22:19:13 +0530

    initial user & ticket model add

---
+++ src/model/Ticket.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+const ticketSchema = new mongoose.Schema(
+  {
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    ticketNumber: { type: String, required: true, unique: true },
+    purchaseDate: { type: Date, default: Date.now },
+    BookedFor: {
+      type: String,
+      enum: ["Primary", "Member"],
+      default: "Primary",
+    },
+    members: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User.members",
+        required: function () {
+          return this.issuedFor === "Member";
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["Active", "Used", "Cancelled", "Expired"],
+      default: "Active",
+    },
+    qrCode: { type: String, required: true }, // URL or base64 of QR code
+  },
+  { timestamps: true }
+);
+ticketSchema.index({ ticketNumber: 1, eventId: 1 });
+
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
+
+export default Ticket;
--- src/model/User.model.js
+++ src/model/User.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
   {
+    //for authantication
     email: {
       type: String,
       required: true,
@@ -12,11 +13,68 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required"],
     },
+    resetToken: {
+      type: String,
+    },
+    resetTokenExpiry: {
+      type: Date,
+    },
+    otpCode: { type: String },
+    otpExpiry: { type: Date },
+    emailVerified: { type: Boolean, default: false },
+    //for personal details
+    /*firstName: { type: String, required: true },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    }, // URL to image
+    phone: {
+      type: String,
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other", "Prefer not to say"],
+    },*/
+    //for member
+    members: [Member],
+    // members: [
+    //   {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "Member",
+    //   },
+    // ],
+    //for ticket
+    tickets: [Ticket],
+    // tickets: {
+    //   type: [mongoose.Schema.Types.ObjectId],
+    //   ref: "Ticket",
+    // },
+    //for event listing
+    //have to make changes according to give details
+    isOrganizer: { type: Boolean, default: false },
+    organizerDetails: {
+      name: { type: String },
+      website: { type: String },
+      description: { type: String },
+      verificationStatus: {
+        type: String,
+        enum: ["Pending", "Verified", "Rejected"],
+        default: "Pending",
+      },
+      documents: [{ type: String }], // URLs to uploaded verification documents
+    },
   },
   { timestamps: true }
 );
+userSchema.index({ email: 1 });
 
-// Export User model (caching applied)
+// Export User model
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
 export default User;
